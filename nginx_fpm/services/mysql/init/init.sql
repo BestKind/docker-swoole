23513196f59090eadfd993f10b686b166ba7368f
田丰 <tianfeng@haohuimai8.com>
2019-07-25 11:57:53 +0800

    add config file

---
+++ nginx_fpm/services/mysql/init/init.sql
@@ -0,0 +1,3 @@
+GRANT All privileges ON *.* TO 'root'@'%' identified by "root" with grant option;
+GRANT All privileges ON *.* TO 'app'@'%' identified by "123456" with grant option;
+flush privileges;
